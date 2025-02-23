6372608656de9edb5019b187ab457eed44ee038f
Paradox <shubhamware555@gmail.com>
2025-02-23 21:43:14 +0530

    formating

--- core/models/ollama.py
+++ core/models/ollama.py
@@ -9,8 +9,8 @@ class OllamaClient:
         self.model_name = model_name
         self.gpu_server = gpu_server
         self.kwargs = kwargs
-        
 
     def get_client(self):
-        return self.ChatOllama(model=self.model_name, gpu_server=self.gpu_server, **self.kwargs)
-           
+        return self.ChatOllama(
+            model=self.model_name, gpu_server=self.gpu_server, **self.kwargs
+        )
