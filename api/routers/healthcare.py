41b5f48fa5b9fcbfb305762aad7b2c5eff30dec6
Paradox <shubhamware555@gmail.com>
2025-03-10 00:00:02 +0530

    added supprot for indexname

--- api/routers/healthcare.py
+++ api/routers/healthcare.py
@@ -2,27 +2,31 @@
 
 from fastapi import APIRouter, HTTPException, UploadFile
 from api.services.healthcare_service import HealthCareService
+
 router = APIRouter(
-    prefix="/healthcare", tags=["healthcare"], responses={404: {"description": "Not found"}}
+    prefix="/healthcare",
+    tags=["healthcare"],
+    responses={404: {"description": "Not found"}},
 )
 
 # Initialize the HealthCareService
 healthcare_service = HealthCareService()
+
 
 # Router to upload a PDF report
 @router.post("/upload-report")
 async def upload_report(file: UploadFile):
     try:
         report = await healthcare_service.upload_report(file)
-        return {"message": "Report uploaded successfully", "report": report}
+        return {"message": "Report uploaded successfully", "index_name": report}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing PDF: {str(e)}")
-    
+
 
 @router.get("/search")
-async def search(query: str):
+async def search(query: str, index_name: str):
     try:
-        result = await healthcare_service.search_agent(query)
+        result = await healthcare_service.search_agent(query, index_name=index_name)
         return {"message": "Search successful", "result": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error searching: {str(e)}")
--- api/services/healthcare_service.py
+++ api/services/healthcare_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from core.agents.document_assistant import DocumentAssistant
 from core.utils.logger import logger
 
@@ -9,16 +10,20 @@ class HealthCareService:
     async def upload_report(self, pdf_file):
         # Call the knowledge creation agent
         file_buffer = await pdf_file.read()
+        # Generate unique index name
+        index_name = f"health_bot_{pdf_file.filename}_time_{datetime.now().strftime('%Y%m%d%H%M%S')}"
         text = self.document_assistant.knowledge_creation_agent(
-            file_buffer, index_name="ruchita"
+            file_buffer, index_name=index_name
         )
         logger.info(f"Knowledge creation agent created knowledge: {text}")
-        return text
+        return index_name
 
-    async def search_agent(self, query):
+    async def search_agent(self, query, index_name):
         logger.info(f"Search agent called with query: {query}")
         # Call the search agent
         system_prompt = """You are a healthcare assistant. You are given a query and a set of documents. Your task is to answer the query based on the documents. You should only use the information provided in the documents to answer the query."""
-        text = self.document_assistant.search_agent(query, index_name="ruchita", system_prompt=system_prompt)
+        text = self.document_assistant.search_agent(
+            query, index_name=index_name, system_prompt=system_prompt
+        )
         logger.info(f"Search agent created knowledge: {text}")
         return text
